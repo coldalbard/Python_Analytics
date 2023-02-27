8d7e44da64206f6c0908d46deb53f070d0883dba
Coldalbard <coldalbard.coldalbard@gmail.com>
2023-02-27 15:01:51 +0300

    added task3(homework2)

---
+++ Homework2/task3.py
@@ -0,0 +1,9 @@
+# Задача 14: Требуется вывести все целые степени двойки 
+# (т.е. числавида 2k), не превосходящие числа N.
+# 10 -> 1 2 4 8
+
+# number = int(input("Please, enter your number: "))
+# k = 8
+# for i in range(1, number + 1):
+#     if k % i == 0:
+#         print(i)
