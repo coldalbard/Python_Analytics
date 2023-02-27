92d07ffe1ba4b9f804659536c8baf8db4b954449
Coldalbard <coldalbard.coldalbard@gmail.com>
2023-02-27 14:38:42 +0300

    added homework1

---
+++ Homework1/task1.py
@@ -0,0 +1,10 @@
+# дз
+# Задача 2: Найдите сумму цифр трехзначного числа
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+# num = int(input("Enter a three-digit number: "))
+# if num < 99 or num > 999:
+#     print("Error, enter a three-digit number:")
+# else:
+#     print(num // 100 + (num // 10) % 10 + num % 10)
