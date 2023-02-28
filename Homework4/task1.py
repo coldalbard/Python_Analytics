d5bcf18372fb675e5d5e4bc238778481b8083758
Coldalbard <coldalbard.coldalbard@gmail.com>
2023-02-28 23:26:58 +0300

    Added new solutions

--- Homework4/task1.py
+++ Homework4/task1.py
@@ -12,11 +12,44 @@ import random
 
 # 1 решение
 
-n = int(input())
-m = int(input())
-array1 = {random.randint(-100, 100) for i in range(n)}
-array2 = {random.randint(-100, 100) for i in range(m)}
-result_array = sorted(array1.union(array2))
-print(array1)
-print(array2)
-print(result_array)
+# n = int(input("Please enter the length of the first array: "))
+# m = int(input("Please enter the length of the second array: "))
+# array1 = {random.randint(-100, 100) for i in range(n)}
+# array2 = {random.randint(-100, 100) for i in range(m)}
+# result_array = sorted(array1.union(array2))
+# print(f'Your first array - {array1}')
+# print(f'Your second array - {array2}')
+# print(f'Numbers that occur in both sets, going from a smaller to a larger value: \n{result_array}')
+
+# 2 решение
+
+# size1 = int(input())
+# size2 = int(input())
+# arr1 = [random.randint(-100, 100) for i in range(size1)]
+# arr2 = [random.randint(-100, 100) for i in range(size2)]
+# res = arr1 + arr2
+# print(f'Your first array - {arr1}')
+# print(f'Your second array - {arr2}')
+# print(f'Numbers that occur in both sets, going from a smaller to a larger value: \n{sorted(set(res))}')
+
+# 3 решение
+
+# mol = [int(x) for x in input().split()]
+# n = mol[0]
+# m = mol[1]
+# set_1 = set()
+# set_2 = set()
+# list_1 = list()
+# a = [int(x) for x in input().split()]
+# k = set(a)
+# for i in k:
+#     set_1.add(i)
+# b = [int(x) for x in input().split()]
+# k1 = set(b)
+# for i in k1:
+#     set_2.add(i)
+# lok = set_1 & set_2
+# kool = list(lok)
+# kool.sort()
+# for i in kool:
+#     print(i, end=' ')
