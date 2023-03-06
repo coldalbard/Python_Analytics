a72a20a878d9c42d9ca14dc15bd8433886b32047
Coldalbard <coldalbard.coldalbard@gmail.com>
2023-03-06 16:38:05 +0300

    added task2(homework5)

---
+++ Homework5/task2.py
@@ -0,0 +1,16 @@
+# Задача 28: Напишите рекурсивную функцию sum(a, b), 
+# возвращающую сумму двух целых неотрицательных чисел. 
+# Из всех арифметических операций допускаются только +1 и -1. 
+# Также нельзя использовать циклы.
+# *Пример:*
+# 2 2
+#     4
+# a = int(input("Please enter the number A: "))
+# b = int(input("Please enter the number B: "))
+# def SumNumbers(a, b):
+#     if 0 == b:
+#         return a
+#     return SumNumbers(a + 1, b - 1)
+# print(f'{a} + {b} = {SumNumbers(a, b)}')
+
+
